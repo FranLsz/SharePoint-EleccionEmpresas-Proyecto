ff1342695b0e8f4fc50f50183bef75a3ac1d0eb5
FranLsz <franlsz95develop@gmail.com>
2016-04-12 14:29:18 +0200

    Creado modelo de Empresa y Alumno

---
+++ SpEleccionEmpresas/Scripts/app/models/alumno.ts
@@ -0,0 +1,21 @@
+﻿export class Empresa {
+
+    public id: number;
+    public nombre: string;
+    public apellidos: string;
+    public puntuacion: number;
+
+    constructor();
+    constructor(nombre: string, apellidos: string, puntuacion: number);
+    constructor(nombre: string, apellidos: string, puntuacion: number, id: number) {
+
+        if (nombre != undefined) {
+            if (id != undefined)
+                this.id = id;
+
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.puntuacion = puntuacion;
+        }
+    }
+}
---
+++ SpEleccionEmpresas/Scripts/app/models/empresa.ts
@@ -0,0 +1,24 @@
+﻿export class Empresa {
+
+    public id: number;
+    public nombre: string;
+    public descripcion: string;
+    public vacantes: number;
+
+    constructor();
+    constructor(nombre: string, descripcion: string, vacantes: number);
+    constructor(nombre: string, descripcion: string, vacantes: number, id: number) {
+
+        if (nombre != undefined) {
+            if (id != undefined)
+                this.id = id;
+
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.vacantes = vacantes;
+        }
+    }
+
+
+
+}
